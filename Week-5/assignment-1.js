a51027a61423acef7a65be725d6d1e21fd69889b
vamsilearner <vkp.in@yahoo.com>
2022-12-14 22:05:05 +0530

    assignment-1 week-5

--- Week-5/assignment-1.js
+++ Week-5/assignment-1.js
@@ -1,27 +1,21 @@
-const foo = function*() { // generator fn
-  yield 'a';  // yield returns the values
-  yield 'b';
-  yield 'c';
-};
+const get = () => 'ElonM@2022'; // 3 functions defined
+const set = () => 'Elon Musk';
+const go = () => 'Silicon Valley, USA';
 
-let str = '';
-for (const val of foo()) { // loop
-  str = str + val;
+async function* mixedFunction() {  //async function and generator function
+    const v1 = await get();       // await waits until it gets its value
+    yield v1;                     // yield returns the value to mixed function
+
+    const v2 = await set();
+    yield v2;
+
+    const v3 = await go();
+    yield v3;
 }
 
-console.log(str);
-
-// async 
-function resolveAfter2Seconds() {  // fn
-    return new Promise(resolve => { 
-        setTimeout(() => {          //async fn
-          resolve('abc');           
-        }, 0);
-    });
-}
-    
-async function asyncCall() {    // sync
-    const result = await resolveAfter2Seconds();  // awaits until executes
-    console.log(result);
-}
-asyncCall();  // calling fn
+const iteration = mixedFunction()  // intializing the function by assigning it to iteration
+iteration.next().then((value) => console.log(value)); // using next() calls mixed function
+iteration.next().then((value) => console.log(value));
+iteration.next().then((value) => console.log(value));
+// here done: true and value: undefined because we have only 3 yielded values after that done : true if you try to executes the function
+iteration.next().then((value) => console.log(value));  
